93cbdac9045bed8f385f00c23999481eb46c8c0e
yifeiche1121 <yifeiche@andrew.cmu.edu>
2022-12-12 16:29:51 -0500

    Update test_single_process_fast.py

--- large_scale_hts_reconciliation/tests/test_single_process_fast.py
+++ large_scale_hts_reconciliation/tests/test_single_process_fast.py
@@ -4,15 +4,15 @@ import pytest
 import itertools
 from collections import defaultdict
 
-DATASETS = ["m5_hobbies", "wikipedia", "m5_full"]
+DATASETS = ["tourism_small"]
 
-hierarchy_prefix = {"m5_hobbies": "m5", "m5_full": "m5", "wikipedia": "wikipedia", "tourism_small": "tourism"}
-num_leaves = {"m5_hobbies": 5650, "m5_full": 30490, "wikipedia": 145063}
-num_nodes = {"m5_hobbies": 6218, "m5_full": 33549, "wikipedia": 308004}
-num_levels = {"m5_hobbies": 4, "m5_full": 4,  "wikipedia": 4}
+hierarchy_prefix = {"tourism_small": "tourism"}
+num_leaves = {"tourism_small": 56}
+num_nodes = {"tourism_small": 89}
+num_levels = {"tourism_small": 4}
 
 
-ROOT = "/home/peiyuan20013/large-scale-hts-reconciliation/large_scale_hts_reconciliation"  # "/data/cmu/large-scale-hts-reconciliation/"
+ROOT = "/home/yifeiche/large-scale-hts-reconciliation/large_scale_hts_reconciliation"  # "/data/cmu/large-scale-hts-reconciliation/"
 data_dir = ROOT + "/notebooks/"
 
 S_compacts = {}
@@ -22,7 +22,7 @@ gts = {}
 yhats = {}
 
 for DATA_ROOT in DATASETS:
-    S_compact = np.load(open(data_dir + DATA_ROOT + "/" + hierarchy_prefix[DATA_ROOT] + "_hierarchy_parent.npy", "rb"))
+    S_compact = np.load(open(data_dir + DATA_ROOT + "/parent.npy", "rb"))
     top_down_p = np.load(open(data_dir + DATA_ROOT + "/top_down_tensor.npy", "rb"))[
         :, 0
     ].reshape(-1, 1)
@@ -30,7 +30,7 @@ for DATA_ROOT in DATASETS:
         :, 0
     ].reshape(-1, 1)
 
-    yhat = np.load(open(data_dir + DATA_ROOT + "/pred_tensor.npy", "rb"))
+    yhat = np.load(open(data_dir + DATA_ROOT + "/yhat_tensor.npy", "rb"))
     gt = np.load(open(data_dir + DATA_ROOT + "/gt_tensor.npy", "rb"))
 
     S_compacts[DATA_ROOT] = S_compact
