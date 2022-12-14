22e62d90c31a0f288674101951ac398460fc37a7
yifeiche1121 <86678948+yifeiche1121@users.noreply.github.com>
2022-12-14 12:41:46 -0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
-import sqlite3
+from mpi4py import MPI
 
 def pytest_configure(config):
-    sqlite3.connect(':memory:')
-    config.pluginmanager.register(
-        'memory_profiler',
-        'pytest_monitor.plugin.MemoryProfiler',
-        '--memory-profile'
-    )
+    if MPI.COMM_WORLD.rank == 0:
+        config.pluginmanager.register(
+            'memory_profiler',
+            'pytest_monitor.plugin.MemoryProfiler',
+            '--memory-profile'
+        )
