61862536fd1205fb36c695eac3cdb9906b1e14c7
yifeiche1121 <86678948+yifeiche1121@users.noreply.github.com>
2022-12-14 12:51:54 -0500

    Delete conftest.py

--- conftest.py
+++
@@ -1,9 +0,0 @@
-from mpi4py import MPI
-
-def pytest_configure(config):
-    if MPI.COMM_WORLD.rank == 0:
-        config.pluginmanager.register(
-            'memory_profiler',
-            'pytest_monitor.plugin.MemoryProfiler',
-            '--memory-profile'
-        )
