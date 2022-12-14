d1732c3535f43b12405d6706ae689fd64441d0f6
yifeiche1121 <yifeiche@andrew.cmu.edu>
2022-12-13 20:09:27 -0500

    Update test_single_process_fast.py

--- large_scale_hts_reconciliation/tests/test_single_process_fast.py
+++ large_scale_hts_reconciliation/tests/test_single_process_fast.py
@@ -2,7 +2,14 @@ import lhts
 import numpy as np
 import pytest
 import itertools
+import sqlite3
+import pandas as pd
 from collections import defaultdict
+
+con = sqlite3.connect(".pymon")
+df = pd.read_sql_query("SELECT * from TEST_METRICS", con)
+df.to_csv('metrics.csv')
+con.close()
 
 DATASETS = ["TourismSmall", "Labour"]
 
